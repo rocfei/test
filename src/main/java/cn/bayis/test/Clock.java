6ae085d2d9dd052c3de487e7fa0888f9ddf6f1c3
backzero <rocfeipeng@163.com>
2016-10-09 15:17:02 +0800

    Created class Clock.

---
+++ src/main/java/cn/bayis/test/Clock.java
@@ -0,0 +1,14 @@
+/*
+ *	Copyright © 2016 Bayi Studio. All rights reserved.
+ *	八裔工作室 版权所有
+ *	http://www.bayis.cn
+ */
+package cn.bayis.test;
+
+/**
+ * @author Penguin
+ *
+ */
+public class Clock {
+
+}
