fca54f70b9940c72a68d8745178ea7f3ccddc356
backzero <rocfeipeng@163.com>
2016-10-09 15:16:04 +0800

    Created and wrote class TimeSubject according to the class diagram.

---
+++ src/main/java/cn/bayis/test/TimeSubject.java
@@ -0,0 +1,25 @@
+/*
+ *	Copyright © 2016 Bayi Studio. All rights reserved.
+ *	八裔工作室 版权所有
+ *	http://www.bayis.cn
+ */
+package cn.bayis.test;
+
+import java.util.Map;
+
+/**
+ * @author Penguin
+ *
+ */
+public abstract class TimeSubject {
+	protected Map<String, Clock> clocks = new HashMap<>();
+	
+	public void attach(String cityName, Clock clock) {
+		clocks.put(cityName, clock);
+	}
+	
+	public void detach(String cityName) {
+		clocks.remove(cityName);
+	}
+	public abstract void notifyAllClocks();
+}
